059c4fdd4465fb69bfdfabaa63766452819e3647
Littlebox <sasha@littlebox.spb.ru>
2024-01-18 23:32:18 +0300

    modified:   K6 Number Pyramid Series 2 - Sum of Edges.py

--- K6 Number Pyramid Series 2 - Sum of Edges.py
+++ K6 Number Pyramid Series 2 - Sum of Edges.py
@@ -1,10 +1,10 @@
 def sum_edges(n):
     i=1 
-    l=1
-    while i<n:
-        l+=i
+    l=(1,0)[n==0]
+    while n>1 and i<=n:
+        l+=(i,i*i+2)[i>1]-1
         i+=1
-    return l        
+    return l
         
 
 
@@ -13,3 +13,7 @@ assert sum_edges(1) == 1
 assert sum_edges(2) == 6
 assert sum_edges(3) == 21
 assert sum_edges(4) == 50
+
+
+
+#lambda i:(i,i*i+2)[i>1]
